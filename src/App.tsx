1541e1f75a8f2a1a94d7c9f96d33c250df56eb33
YuanyuanZ07 <W0504770@nscc.ca>
2026-04-10 23:03:34 -0300

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/online-store-react-frontend-YuanyuanZ07">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
